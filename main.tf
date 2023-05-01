c71aeb9953cdbc24c76580efa61d7b8ad35f0ca7
monika-05 <sharma.moni12457@gmail.com>
2023-05-01 19:34:37 +0000

    add security group and user data

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+
+terraform {
+      required_providers {
+         aws = {
+         source = "hashicorp/aws"
+         version = "= 4.65.0"
+        }
+     }
+  }
+
 provider  "aws" {
   region        = "us-east-1"
 }
@@ -5,8 +15,28 @@ provider  "aws" {
 resource "aws_instance"  "instance1" {
   ami           = "ami-016eb5d644c333ccb" 
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags  = { 
      name = "web-server"
+  }
+}
+
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
